05fe72aa229e30921058407853d7caf3165851f6
firyafaturahman <firyafaturahman@gmail.com>
2023-05-29 22:02:05 +0700

    sesi 4 dan tugas 4

---
+++ Sesi-04/tugas4/js/script.js
@@ -0,0 +1,48 @@
+// * Luas lingkaran:
+// Flow:
+//   1. masukan ukuran jari-jari lingkaran
+//   2. rumus luas lingkaran = phi * r * r
+//   3. kalkulasi luas lingkaran sesuai rumus
+//   4. output hasil perhitungan luas lingkaran
+
+// Pseudocode:
+
+//   let r, luasLingkaran, phi
+
+//   r = prompt('ukuran jari-jari: ')
+//   phi = Math.PI
+//   luasLingkaran = phi * r * r
+//   console.log(luasLingkaran)
+
+// let r;
+// let luasLingkaran;
+// let phi;
+
+// r = prompt("ukuran jari-jari: ");
+// phi = Math.PI;
+// luasLingkaran = phi * r * r;
+// console.log(luasLingkaran.toFixed());
+
+// ** Keliling lingkaran:
+// Flow:
+//   1. masukan ukuran jari-jari lingkaran
+//   2. rumus keliling lingkaran = 2 * phi * r
+//   3. kalkulasi keliling lingkaran sesuai rumus
+//   4. output hasil perhitungan keliling lingkaran
+
+// Pseudocode:
+//   let r, kelilingLingkaran, phi
+
+//   r = prompt('ukuran jari-jari: ')
+//   phi = prompt('nilai phi: ')
+//   kelilingLingkaran = 2 * phi * r
+//   console.log(kelilingLingkaran)
+
+let r;
+let kelilingLingkaran;
+let phi;
+
+r = prompt("ukuran jari-jari: ");
+phi = Math.PI;
+kelilingLingkaran = 2 * phi * r;
+console.log(kelilingLingkaran.toFixed());
